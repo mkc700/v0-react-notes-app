4d4870f98008fc57e620ae81ede330752979ffc2
Miguel Angel <youngpl3asant@gmail.com>
2025-10-28 18:02:48 -0600

    actualizacion_0.001

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NotesApp } from "@/components/notes-app"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background">
-      <NotesApp />
-    </main>
-  )
-}
